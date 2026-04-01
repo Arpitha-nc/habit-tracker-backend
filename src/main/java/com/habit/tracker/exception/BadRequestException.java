114a5553604cc25464e9f2f3b6f23786b32c9da1
Arpitha-nc <72622082+Arpitha-nc@users.noreply.github.com>
2026-04-01 20:12:41 +0530

    Exception and Logging (#4)
    * feat: implement global exception handling and request logging

---
+++ src/main/java/com/habit/tracker/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.habit.tracker.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
